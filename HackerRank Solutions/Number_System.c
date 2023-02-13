ccd30b137f43a962e0ab6229f5b7096141d1f919
NGDEC03 <ngdec03@gmail.com>
2023-02-13 23:07:18 +0530

    main

---
+++ HackerRank Solutions/Number_System.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+int val;
+    scanf("%d\n",&val);
+    printf("%d\n",val);
+    printf("%o\n",val);
+    printf("%x\n",val);
+    printf("%X\n",val);
+    return 0;
+}
