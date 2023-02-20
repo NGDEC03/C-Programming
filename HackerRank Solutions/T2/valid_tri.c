1743066d119b1e1499bde5ae6ef4c700d05932b6
NGDEC03 <ngdec03@gmail.com>
2023-02-20 10:08:30 +0530

    main

---
+++ HackerRank Solutions/T2/valid_tri.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main() {
+int t,a,b,c;
+    scanf("%d",&t);
+    for(int i=0;i<t;i++){
+        scanf("%d %d %d",&a,&b,&c);
+        if(a+b+c==180){
+            printf("YES\n");
+        }
+        else{
+            printf("NO\n");
+        }
+    }
+    
+    return 0;
+}
